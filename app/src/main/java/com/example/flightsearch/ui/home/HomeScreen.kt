37141409c4f0ae51c70d1e85b3f7818753ba9581
Dylan Trepos <dylan.trepos@gmail.com>
2024-12-08 13:00:35 +0100

    Clean unused imports

--- app/src/main/java/com/example/flightsearch/ui/home/HomeScreen.kt
+++ app/src/main/java/com/example/flightsearch/ui/home/HomeScreen.kt
@@ -1,11 +1,7 @@
 package com.example.flightsearch.ui.home
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -13,8 +9,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.res.dimensionResource
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import com.example.flightsearch.R
 
